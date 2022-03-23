8175b06c4c89f95e0f215f8200c53f87c2201bcb
Fernando Segregio <segregio@gmail.com>
2022-03-23 05:45:29 -0300

    style: adiciona estilo no footer.

--- src/components/FooterPage/style.js
+++ src/components/FooterPage/style.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 
 export const FooterStyle = styled.footer`
-    
+  align-items: center;
+  color: var(--black-200);
+  display: flex;
+  font-family: var(--unnamed-font-family-montserrat);
+  font-size: var(--unnamed-font-size-25);
+  font-style: var(--unnamed-font-style-normal);
+  font-weight: var(--unnamed-font-weight-bold); 
+  line-height: var(--unnamed-line-spacing-30); 
+  justify-content: center;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  padding: 20px;
 `;
 
 export const ButtonStyle = styled.button`
-    
+  border: none;
+  padding: 20px;
+  background-color: transparent;
+  font-size: 20px;
 `;
 
