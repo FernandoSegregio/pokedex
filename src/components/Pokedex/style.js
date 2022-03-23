a7e179f94f17a35e1f13bb8c93ab0a58de9b0874
Fernando Segregio <segregio@gmail.com>
2022-03-23 03:18:43 -0300

    style: adiciona css para a pokedex de acordo com o prototipo.

--- src/components/Pokedex/style.js
+++ src/components/Pokedex/style.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
 
 export const PokedexStyle = styled.div`
-    
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 0 100px;
+  place-items: center;
+  margin-top: 38px;
 `;
 
 export const CardStyle = styled.div`
-    
+
+`;
+export const NameStyle = styled.span`
+
 `;
 
-export const TypeStyle = styled.div`
-    
+export const TypesStyle = styled.div`
+
+`;
+
+export const TypeStyle = styled.span`
+
 `;
 
 export const IdStyle = styled.p`
-    
+ 
 `;
 
 export const ImgStyle = styled.img`
-    
+
 `;
