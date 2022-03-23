aa4e313e0482dc3c4a3cced1280525dfa6a29dcb
Fernando Segregio <segregio@gmail.com>
2022-03-23 03:17:45 -0300

    refactor: acrescenta styled component para name e type.

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,33 +1,38 @@
 import React, { useContext } from 'react';
 import PokedexContext from '../../context/PokedexContext';
-import  { PokedexStyle, CardStyle, TypeStyle, IdStyle, ImgStyle } from './style';
+import {
+	PokedexStyle,
+	CardStyle,
+	IdStyle,
+	ImgStyle,
+	NameStyle,
+	TypesStyle,
+	TypeStyle,
+} from './style';
 
 function Pokedex() {
-	const {  arrayPokemons } = useContext(PokedexContext);
-	
+	const { arrayPokemons } = useContext(PokedexContext);
+
 	return (
 		<PokedexStyle>
-			{ arrayPokemons && arrayPokemons.map(( {data}, i) => {
-				return (
-					<CardStyle key={ i }>
-						<span>{data.name}</span>
-						<TypeStyle>
-							{ data.types.map(( {type}, i) => {
-								return (
-									<span key={i}>
-										{type.name}
-									</span>
-								);
-							})}
-						</TypeStyle>
-						<IdStyle>#{('000' + Number(data.id)).slice(-3)}</IdStyle>
-						<ImgStyle
-							src={data.sprites.other.home.front_default}
-							alt={data.name}
-						/>
-					</CardStyle>
-				);
-			}) }
+			{arrayPokemons &&
+		arrayPokemons.map(({ data }, i) => {
+			return (
+				<CardStyle key={i}>
+					<NameStyle>{data.name}</NameStyle>
+					<TypesStyle>
+						{data.types.map(({ type }, i) => {
+							return <TypeStyle key={i}>{type.name}</TypeStyle>;
+						})}
+					</TypesStyle>
+					<IdStyle>#{('000' + Number(data.id)).slice(-3)}</IdStyle>
+					<ImgStyle
+						src={data.sprites.other.home.front_default}
+						alt={data.name}
+					/>
+				</CardStyle>
+			);
+		})}
 		</PokedexStyle>
 	);
 }
