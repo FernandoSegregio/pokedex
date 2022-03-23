4c745928b90f030291e636886545fea2504c0e60
Fernando Segregio <segregio@gmail.com>
2022-03-23 02:04:30 -0300

    feat: calcula o total de paginas de acordo com o total de pokemons que retorna da API.

--- src/context/PokedexProvider.jsx
+++ src/context/PokedexProvider.jsx
@@ -5,18 +5,20 @@ import { api, getListPokemonsApi } from '../services/api';
 
 function PokedexProvider({ children }) {
 	const [arrayPokemons, setArrayPokemons] = useState([]);
+	const [totalPage, setTotalPage] = useState(0);
 	
 	const pokemonsPerPage = 16;
 
 	async function getPokemonData() {
 		try {
-			const { results } = await getListPokemonsApi(pokemonsPerPage, 0);
+			const { results, count } = await getListPokemonsApi(pokemonsPerPage, 0);
 			
 			const promises = results.map(async (pokemon) => {
 				return await api.get(pokemon.url.split('pokemon', 2)[1]);
 			});
 			const result = await Promise.all(promises);
 			setArrayPokemons(result);
+			setTotalPage(Math.ceil(count / pokemonsPerPage));
 
 		} catch (error) {
 			console.log('error: ', error);
@@ -32,6 +34,7 @@ function PokedexProvider({ children }) {
 
 	const context = {
 		arrayPokemons,
+		totalPage,
 	};
 
 	return (
