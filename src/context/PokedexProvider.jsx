f5392c9bafa07e6c91ca82567ef6ff3354345be6
Fernando Segregio <segregio@gmail.com>
2022-03-23 01:57:56 -0300

    feat: adiciona no Provider requisicao a API, executa requisição ao carregar a pagina, disponibiliza resultado pra aplicacao.

--- src/context/PokedexProvider.jsx
+++ src/context/PokedexProvider.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PokedexContext from './PokedexContext';
+import { api, getListPokemonsApi } from '../services/api';
 
 function PokedexProvider({ children }) {
-	const context = { 
-    
+	const [arrayPokemons, setArrayPokemons] = useState([]);
+	
+	const pokemonsPerPage = 16;
+
+	async function getPokemonData() {
+		try {
+			const { results } = await getListPokemonsApi(pokemonsPerPage, 0);
+			
+			const promises = results.map(async (pokemon) => {
+				return await api.get(pokemon.url.split('pokemon', 2)[1]);
+			});
+			const result = await Promise.all(promises);
+			setArrayPokemons(result);
+
+		} catch (error) {
+			console.log('error: ', error);
+		}
+	}
+
+
+	useEffect(() => {
+		getPokemonData();
+	}, []);
+
+
+
+	const context = {
+		arrayPokemons,
 	};
 
 	return (
