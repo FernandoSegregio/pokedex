acfd99f2b5617f21f41c6a26f8af2a04179fe088
Fernando Segregio <segregio@gmail.com>
2022-03-23 02:06:43 -0300

    feat: cria styled componentes para ser renderizados no componente Pokedex.

---
+++ src/components/Pokedex/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const PokedexStyle = styled.div`
+    
+`;
+
+export const CardStyle = styled.div`
+    
+`;
+
+export const TypeStyle = styled.div`
+    
+`;
+
+export const IdStyle = styled.p`
+    
+`;
+
+export const ImgStyle = styled.img`
+    
+`;
