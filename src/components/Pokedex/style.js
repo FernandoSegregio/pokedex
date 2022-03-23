9b0c4f69903f00a78ec214b3e2aa8f9044a65264
Fernando Segregio <segregio@gmail.com>
2022-03-23 03:19:21 -0300

    style: adiciona css para o card da pokedex de acordo com o prototipo.

--- src/components/Pokedex/style.js
+++ src/components/Pokedex/style.js
@@ -10,7 +10,13 @@ export const PokedexStyle = styled.div`
 `;
 
 export const CardStyle = styled.div`
-
+  border-radius: 13px;
+  box-shadow: 0px 3px 6px #00000029;
+  display: flex;
+  height: 221px;
+  opacity: 1;
+  position: relative;
+  width: 295px;
 `;
 export const NameStyle = styled.span`
 
