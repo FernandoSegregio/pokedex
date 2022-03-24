fa20700cebb6e4604ac1f8c1903841eea529086d
Fernando Segregio <segregio@gmail.com>
2022-03-24 17:37:59 -0300

    style: cria componente para renderizar o nome, o tipo e os backgrounds de cada pokemon. Reposiciona Carousel.

--- src/components/PokemonDetails/style.js
+++ src/components/PokemonDetails/style.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-// import { background } from '../../style/cardColorBase';
+import { background, details } from '../../style/cardColorBase';
 import arrowBack from '../../images/arrow-back.svg';
 
 export const PokemonDetailsContainerStyle = styled.div`
-  background-color: var(--green-100);
+  background-color: ${({defaultValue}) => background[defaultValue]};
   height: 823px;
 `;
 
@@ -14,17 +14,18 @@ export const ButtonPokemonSelectStyle = styled.button`
     cursor: pointer;
     &:nth-child(odd){
       opacity: 0.6;
+      position: relative;
+      top: -26px;
     }
     &:nth-child(even){
       img{
-        height: 400px;
+        height: 330px;
       }
     }
     
 
     img {
-     
-      height: 243px;
+      height: 210px;
     }
 
 `;
@@ -34,7 +35,7 @@ export const CarouselStyle = styled.div`
     justify-content: center;
     position: relative;
     top: 100px;
-    z-index: 1; 
+    z-index: 2;    
 `;
 
 export const PokemonDetailsStyle = styled.div`
@@ -56,4 +57,53 @@ export const ArrowStyle = styled.button`
   position: absolute;
   top: 25px;
   left: 35px;
+`;
+
+export const TypesStyle = styled.div`
+  display: flex;
+  column-gap: 7px;
+`;
+
+export const TypeStyle = styled.span`
+  background-color: ${({defaultValue}) => details[defaultValue]};
+  border-radius: 12px;
+  color: var(--white);
+  font-family: var(--unnamed-font-family-montserrat);
+  font-size: var(--unnamed-font-size-12);
+  font-style: var(--unnamed-font-style-normal) normal;
+  font-weight: var(--unnamed-font-weight-normal);
+  letter-spacing: var(--unnamed-character-spacing-0);
+  line-height: var(--unnamed-line-spacing-15);
+  opacity: 1;
+  padding: 3px 14px;
+  text-align: left;
+`;
+
+export const NameStyle = styled.span`
+  color: var(--white);
+  font-family: var(--unnamed-font-family-montserrat);
+  font-size: var(--unnamed-font-size-33);
+  font-style:var(--unnamed-font-style-normal) normal;
+  font-weight: var(--unnamed-font-weight-bold); 
+  letter-spacing: var(--unnamed-character-spacing-0);
+  line-height: var(--unnamed-line-spacing-40); 
+  opacity: 1;
+`;
+export const ImgBackGroundCardStyle = styled.img`
+  height: 281px;
+  left: 588px;
+  position: absolute;
+  top: 173px;
+  width: 277px;
+  z-index: 1;
+`;
+
+export const NameDatailsPokemonStyle = styled.div`
+  position: absolute;
+  top: 68px;
+  left: 636px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 6px;
 `;
