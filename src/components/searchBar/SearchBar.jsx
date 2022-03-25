19abd04c434b0cc6ccebbf78c1fe0a8c879c523e
Fernando Segregio <segregio@gmail.com>
2022-03-25 00:04:25 -0300

    refactor: troca nome das funcoes para padronizar todas que vem do services com mesmo nome.

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,22 +1,22 @@
 import React, { useContext, useState } from 'react';
 import {SearchBarStyle } from './style';
 import PokedexContext from '../../context/PokedexContext';
-import { getPokemon } from '../../services/api';
+import { getPokemonApi } from '../../services/api';
 
 function SearchBar() {
 	const [pokemonName, setPokemonName] = useState('');
 	const { setIdPokemonOnClick, setIsDetailsClick } = useContext(PokedexContext);
     
-	async function getPokemonApi(e){
+	async function getPokemon(e){
 		e.preventDefault();
-		const result = await getPokemon(pokemonName.toLocaleLowerCase());
+		const result = await getPokemonApi(pokemonName.toLocaleLowerCase());
 		await setIdPokemonOnClick(result.data.id);
 		setIsDetailsClick(true);
 	}
 
 
 	return (
-		<form onSubmit={ (e) => getPokemonApi(e) }>
+		<form onSubmit={ (e) => getPokemon(e) }>
 			<SearchBarStyle 
 				type="text" 
 				placeholder='Busque um pokemon' 
